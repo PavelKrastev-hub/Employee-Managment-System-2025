f3b7aba4e364ff6456335962e95310fd18bad310
PavelKrastev-hub <pavel__krustev@abv.bg>
2025-11-11 21:03:11 +0200

    Show create user modal

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -120,7 +120,6 @@ export default function UserList() {
                 </tbody>
             </table>
 
-            <button className="btn-add btn">Add new user</button>
         </div>
     );
 }
